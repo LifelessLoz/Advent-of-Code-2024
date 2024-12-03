8c59b6412f354efd545755d4a928542ea2088c6a
LifelessLoz <38470405+LifelessLoz@users.noreply.github.com>
2024-12-03 12:06:27 +0000

    Day 3

--- advent_3.cpp
+++ advent_3.cpp
@@ -10,8 +10,8 @@ int main()
     std::string line;
     std::string checkPrefix;
     std::regex rgx("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-    std::regex rgx_dont("\\)\\(t'nod");
-    std::regex rgx_doit("\\)\\(od");
+    std::regex rgx_dont("don't\\(\\)");
+    std::regex rgx_doit("do\\(\\)");
     double answer_p1 = 0;
     double answer_p2 = 0;
     bool enabled = true;
@@ -25,14 +25,13 @@ int main()
             std::smatch m_dont;
             while(std::regex_search(line, m, rgx)){
                 checkPrefix = m.prefix().str();
-                std::reverse(checkPrefix.begin(), checkPrefix.end());
                 std::regex_search(checkPrefix, m_doit, rgx_doit);
                 std::regex_search(checkPrefix, m_dont, rgx_dont);
                 if (m_doit[0] == "" && m_dont[0] != "") enabled = false;
                 else if (m_doit[0] != "" && m_dont[0] == "") enabled = true;
                 else {
-                    if (m_doit.prefix().str().size() > m_dont.prefix().str().size()) enabled = false;
-                    else if (m_dont.prefix().str().size() > m_doit.prefix().str().size()) enabled = true;
+                    if (m_doit.suffix().str().size() > m_dont.suffix().str().size()) enabled = false;
+                    else if (m_dont.suffix().str().size() > m_doit.suffix().str().size()) enabled = true;
                 }
                 if (enabled) answer_p2 += (std::stoi(m[1]) * std::stoi(m[2]));
                 answer_p1 += (std::stoi(m[1]) * std::stoi(m[2]));
